ebcc640b551204f96300fe7564c062fe57040db6
Robinson Amianda <robinsonamianda5@gmail.com>
2023-09-18 19:28:23 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance > 2000 && distance < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } 
+    else {
+      return 'No can do.';
+    }
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+  if(when the city is NYC){
+    returns "Ok, sounds good."
+  }else if(the destination city is not NYC){
+    return "No go."
+  }
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
